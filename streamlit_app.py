936c1a6117b002d92523d50d5e06ed1e2f6e36e5
Samer Haidar <samerhaidar0811@gmail.com>
2023-10-02 12:37:18 +0400

    Add files via upload

---
+++ streamlit_app.py
@@ -0,0 +1,31 @@
+import streamlit as st
+import pandas as pd
+import io
+
+def main():
+    st.title("Excel Manipulation App")
+    
+    # File upload
+    uploaded_file = st.file_uploader("Upload an Excel file", type=["xlsx"])
+
+    if uploaded_file is not None:
+        # Read the uploaded file into a Pandas DataFrame
+        df = pd.read_excel(uploaded_file)
+
+        # Perform data manipulations here (e.g., df manipulation)
+
+        # Offer a download link for the manipulated data
+        output = io.BytesIO()
+        with pd.ExcelWriter(output, engine="xlsxwriter") as writer:
+            df.to_excel(writer, sheet_name="Sheet1", index=False)
+
+        output.seek(0)
+        st.download_button(
+            label="Download Manipulated Data",
+            data=output,
+            file_name="manipulated_data.xlsx",
+            key="download_button"
+        )
+
+if __name__ == "__main__":
+    main()

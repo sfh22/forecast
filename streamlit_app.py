61921b93c40fbe37f4ce68cbf299fa45053ee7d0
Samer Haidar <samerhaidar0811@gmail.com>
2023-10-02 15:25:46 +0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,74 +5,75 @@ def main():
     st.title("Excel Manipulation and Download App")
     
     # File upload
-    uploaded_file = st.file_uploader("Upload an Excel file", type=["xlsx"])
+    uploaded_files = st.file_uploader("Upload Excel files", type=["xlsx"], accept_multiple_files=True)
 
-    if uploaded_file is not None:
-        # Read the uploaded file into a Pandas DataFrame
-        df = pd.read_excel(uploaded_file, sheet_name=['PM GCC', 'PM Egypt', 'PM Levant', 'PM Iraq'])
-
+    if uploaded_files:
         # Create an empty list to store DataFrames for all files
         all_dfs = []
+        
+        for uploaded_file in uploaded_files:
+            # Read the uploaded file into a Pandas DataFrame
+            df = pd.read_excel(uploaded_file, sheet_name=['PM GCC', 'PM Egypt', 'PM Levant', 'PM Iraq'])
 
-        # List of agency names to look for
-        agency_names = ['Starcom', 'Zenith', 'Spark', 'Digitas', 'Publicis']
+            # List of agency names to look for
+            agency_names = ['Starcom', 'Zenith', 'Spark', 'Digitas', 'Publicis']
 
-        for sheet_name, df_sheet in df.items():  # Iterate through specified sheets
-            # Read the sheet with 'Client' as the header row
-            df = pd.read_excel(uploaded_file, sheet_name=sheet_name, header=None)
+            for sheet_name, df_sheet in df.items():  # Iterate through specified sheets
+                # Read the sheet with 'Client' as the header row
+                df = pd.read_excel(uploaded_file, sheet_name=sheet_name, header=None)
 
-            # Find the row with 'Client' and set it as the header
-            header_row = df[df.apply(lambda row: row.astype(str).str.contains('Client').any(), axis=1)].index[0]
-            df.columns = df.iloc[header_row]
+                # Find the row with 'Client' and set it as the header
+                header_row = df[df.apply(lambda row: row.astype(str).str.contains('Client').any(), axis=1)].index[0]
+                df.columns = df.iloc[header_row]
 
-            # Delete rows above the 'Client' row
-            df = df.iloc[header_row+1:]
+                # Delete rows above the 'Client' row
+                df = df.iloc[header_row+1:]
 
-            # Reset the index
-            df.reset_index(drop=True, inplace=True)
+                # Reset the index
+                df.reset_index(drop=True, inplace=True)
 
-            agency_name = None
+                agency_name = None
 
-            # Filter out columns containing the word "Total," excluding 'Total MBC'
-            columns_to_drop = [col for col in df.columns if 'Total' in str(col) and col != 'Total MBC']
-            df = df.drop(columns=columns_to_drop)
+                # Filter out columns containing the word "Total," excluding 'Total MBC'
+                columns_to_drop = [col for col in df.columns if 'Total' in str(col) and col != 'Total MBC']
+                df = df.drop(columns=columns_to_drop)
 
-            # Filter out rows where 'Client' contains the word "Total" and is not NaN
-            df = df[~df['Client'].str.contains('Total', na=False)]
+                # Filter out rows where 'Client' contains the word "Total" and is not NaN
+                df = df[~df['Client'].str.contains('Total', na=False)]
 
-            # Iterate through the rows to find and store agency names
-            for index, row in df.iterrows():
-                if any(name in str(row['Client']) for name in agency_names):
-                    agency_name = row['Client']
-                    df.at[index, 'Client'] = None
-                elif row['Client'] == 'Total':
-                    agency_name = None
-                elif agency_name:
-                    df.at[index, 'Agency'] = agency_name
+                # Iterate through the rows to find and store agency names
+                for index, row in df.iterrows():
+                    if any(name in str(row['Client']) for name in agency_names):
+                        agency_name = row['Client']
+                        df.at[index, 'Client'] = None
+                    elif row['Client'] == 'Total':
+                        agency_name = None
+                    elif agency_name:
+                        df.at[index, 'Agency'] = agency_name
 
-            # Drop rows where both 'Client' and 'Agency' are None
-            df = df.dropna(subset=['Client', 'Agency'], how='all')
+                # Drop rows where both 'Client' and 'Agency' are None
+                df = df.dropna(subset=['Client', 'Agency'], how='all')
 
-            # Reset the index
-            df.reset_index(drop=True, inplace=True)
+                # Reset the index
+                df.reset_index(drop=True, inplace=True)
 
-            # Remove empty rows
-            df = df.dropna(how='all')
+                # Remove empty rows
+                df = df.dropna(how='all')
 
-            # Melt the DataFrame
-            df = df.melt(id_vars=['Client', 'Agency'], var_name='Channel', value_name='Value')
+                # Melt the DataFrame
+                df = df.melt(id_vars=['Client', 'Agency'], var_name='Channel', value_name='Value')
 
-            # Add the 'FileName' and 'SheetName' columns
-            df['FileName'] = sheet_name
-            df['SheetName'] = sheet_name
+                # Add the 'FileName' and 'SheetName' columns
+                df['FileName'] = sheet_name
+                df['SheetName'] = sheet_name
 
-            # Assign 'Currency' based on sheet name
-            df['Currency'] = 'EGP' if sheet_name == 'PM Egypt' else 'USD'
+                # Assign 'Currency' based on sheet name
+                df['Currency'] = 'EGP' if sheet_name == 'PM Egypt' else 'USD'
 
-            # Append the DataFrame to the list
-            all_dfs.append(df)
+                # Append the DataFrame to the list
+                all_dfs.append(df)
 
-        # Concatenate all DataFrames from different sheets into a single DataFrame
+        # Concatenate all DataFrames from different files into a single DataFrame
         final_result_df = pd.concat(all_dfs, ignore_index=True)
 
         # Reorder the columns in final_result_df
